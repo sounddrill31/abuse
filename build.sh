548d37921085b49e66ed8ef23272d428dd3ea4a2
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-08-11 06:27:54 +0000

    Compile abuse-tool seperately Both have main function

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if ! command -v emcc &> /dev/null; then
-  bash ~/emsdk/setupenv.sh
+  bash setupenv.sh
+  source ~/emsdk/emsdk_env.sh
 fi
 
 # Create a build directory for Emscripten
@@ -20,10 +21,34 @@ cd build_emscripten
 # Compile to JavaScript
 echo "Compiling to JavaScript..."
 
+# Compile abuse-tool separately
+emcc -DABUSE_TOOL ../src/tool/abuse-tool.cpp -o abuse-tool.js -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 -s USE_FREETYPE=1 -s USE_SDL_MIXER=2 -s USE_OGG=1 -std=c++14
+
 # Compile with Emscripten
 #emcc -o abuse.html ../src/game.cpp \
 #    -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_MIXER=2 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1
+# Define an array of directory paths
+directories=(
+    "../src"
+    "../src/imlib"
+    "../src/lisp"
+    "../src/lol"
+    "../src/net"
+    "../src/sdlport"
+    "../src/ui"
+)
+# Initialize an empty string to store all source files
+source_files=""
 
- emcc ../src/*.cpp ../src/*/*.cpp -o test.html -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 -s USE_FREETYPE=1 -s USE_SDL_MIXER=2 -s USE_OGG=1 -std=c++14
+# Accumulate all source files
+for dir in "${directories[@]}"; do
+    source_files+="$dir/*.cpp "
+done
+
+# Compile all files in one go
+emcc $source_files -o test.html -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 -s USE_FREETYPE=1 -s USE_SDL_MIXER=2 -s USE_OGG=1 -std=c++14 --emrun
+
+# Link all object files
+# emcc *.o -o test.html -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 -s USE_FREETYPE=1 -s USE_SDL_MIXER=2 -s USE_OGG=1 -std=c++14 --emrun
 
 echo "Compilation complete."
