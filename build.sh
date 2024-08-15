92819178809a78080f0b40869f80aaa9effbd093
Uv <uv@crave.io>
2024-08-15 22:34:56 +0000

    make it possible to run the build inside the docker container

--- build.sh
+++ build.sh
@@ -2,22 +2,30 @@
 
 set -e
 
-if ! command -v emcc &> /dev/null; then
-  bash setupenv.sh
-  source ~/emsdk/emsdk_env.sh
+if [ ! $(which emcc) ] ; then
+    if [ -z ${EMSDK} ] ; then
+        # This isn't docker
+        source setupenv.sh
+    else
+        # This is inside docker
+        EMSDK_QUIET=1 source ${EMSDK}/emsdk_env.sh
+    fi
 fi
 
 echo "Starting build"
 
-mkdir -p ../build
-mkdir -p ../install
+# go to the directory that holds this script.. aka the top of the git repo
+cd $(dirname $(readlink -f $0))
+mkdir -p build
+mkdir -p install
+cd build
 
 export EmscriptenRoot=$(dirname $(whereis emcc | cut -d' ' -f2))""
 echo "Configuring Makefiles"
 emcmake cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
     -DCMAKE_TOOLCHAIN_FILE=$EmscriptenRoot/cmake/Modules/Platform/Emscripten.cmake \
     -DEMSCRIPTEN_ROOT_PATH=$EmscriptenRoot \
-    .
+    ..
 
 echo "Building"
 emmake make
